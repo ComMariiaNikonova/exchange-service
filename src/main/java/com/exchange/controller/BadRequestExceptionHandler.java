542adb7765f9dfdcb0174d47798c7c11da6c1506
mnikonova <mnikonova@Mariias-MacBook-Pro.local>
2020-03-17 02:55:09 +0200

    setup small issue fixed

--- src/main/java/com/exchange/controller/BadRequestExceptionHandler.java
+++ src/main/java/com/exchange/controller/BadRequestExceptionHandler.java
@@ -2,7 +2,6 @@ package com.exchange.controller;
 
 import com.exchange.model.ClientError;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,9 +13,7 @@ public class BadRequestExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity handleConflict(IllegalArgumentException e) {
-
-        return new ResponseEntity(new ClientError(e.getCause().getMessage()),
-                HttpStatus.BAD_REQUEST);
+    public ClientError handleConflict(IllegalArgumentException e) {
+        return new ClientError(e.getCause().getMessage());
     }
 }
